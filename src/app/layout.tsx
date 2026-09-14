9d8d8c567a383aaca5abd617a027e9d0bccadb3f
vivekcsein <vivekcse.in@gmail.com>
2026-09-14 11:01:08 +0530

    shadcn ui init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import AppClientLayout from "@/components/layouts/AppClientLayout";
 import { fontMono, fontSans, fontSerif } from "@/packages/configs/fonts.config";
 import { seo } from "@/packages/seo/seo.index";
+import { cn } from "@/packages/utils/cn";
 
 export const metadata: Metadata = seo;
 
@@ -11,7 +12,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       data-scroll-behavior="smooth"
-      className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        fontSans.variable,
+        fontSerif.variable,
+        fontMono.variable,
+        "font-sans",
+      )}
     >
       <body className="min-h-full flex flex-col">
         <AppClientLayout>{children}</AppClientLayout>
--- src/components/layouts/AppClientLayout.tsx
+++ src/components/layouts/AppClientLayout.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { NavigationProvider } from "../providers/NavigationProvider";
 import { ThemeProvider } from "../providers/ThemeProvider";
+import { Toaster } from "../ui/shadcn/toast";
+import { TooltipProvider } from "../ui/shadcn/tooltip";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -12,11 +14,14 @@ const AppClientLayout = (props: AppClientLayoutProps) => {
   const { children } = props;
   return (
     <ThemeProvider>
-      <NavigationProvider>
-        <Header />
-        <main className="min-h-screen w-full">{children}</main>
-        <Footer />
-      </NavigationProvider>
+      <TooltipProvider>
+        <NavigationProvider>
+          <Header />
+          <main className="min-h-screen w-full">{children}</main>
+          <Footer />
+          <Toaster />
+        </NavigationProvider>
+      </TooltipProvider>
     </ThemeProvider>
   );
 };
